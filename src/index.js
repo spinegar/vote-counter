d74e4705ee60db0b315f508dadc97f07fd77add5
Sean Pinegar <spinegar@gmail.com>
2020-08-08 14:18:05 -0500

    require lodash and add orderBy

--- src/index.js
+++ src/index.js
@@ -2,52 +2,56 @@
 
 'use strict';
 
+const _ = require('lodash');
+
 module.exports = function(ballots) {
 
-   const opts = ballots.reduce((memo, ballot) => {
-      ballot.forEach((opt) => {
-         if (!memo.includes(opt)) {
-            memo.push(opt);
-         }
-      });
-      return memo;
-   }, []);
-
-   // Get the value of each option in each ballot
-   const processed = ballots.map((ballot) => {
-      return ballot.reduce((memo, vote, index) => {
-         // Vote should be N-{position}. So first place is N-1 and last is 0.
-         memo[vote] = opts.length - index - 1;
-         return memo;
-      }, {});
-   });
-
-   // Add up the value of each option globally
-   const counted = opts.reduce((memo, opt) => {
-      const value = processed.map((o) => { return o[opt] })
-         .reduce((count, val) => {
-            if (!val) {
-               return count;
+    const opts = ballots.reduce((memo, ballot) => {
+        ballot.forEach((opt) => {
+            if (!memo.includes(opt)) {
+                memo.push(opt);
             }
-            return count + val;
-         }, 0)
+        });
+        return memo;
+    }, []);
 
-      memo.push({ option: opt, value: value });
-      return memo;
-   }, []);
+    // Get the value of each option in each ballot
+    const processed = ballots.map((ballot) => {
+        return ballot.reduce((memo, vote, index) => {
+            // Vote should be N-{position}. So first place is N-1 and last is 0.
+            memo[vote] = opts.length - index - 1;
+            return memo;
+        }, {});
+    });
+
+    // Add up the value of each option globally
+    const counted = opts.reduce((memo, opt) => {
+        const value = processed.map((o) => { return o[opt] })
+            .reduce((count, val) => {
+                if (!val) {
+                    return count;
+                }
+                return count + val;
+            }, 0)
+
+        memo.push({ option: opt, value: value });
+        return memo;
+    }, []);
 
 
-   const winner = counted.map((win, current) => {
-      if (current.value > win.value) {
-         return current;
-      }
+    let winners = counted.map((win, current) => {
+        if (current.value > win.value) {
+            return current;
+        }
 
-      return win;
-   }, { value: 0 });
+        return win;
+    }, { value: 0 });
 
-   return {
-      winner: winner,
-      processedBallets: processed,
-      optionValues: counted,
-   };
+    winners = _.orderBy(winners, 'value', 'desc');
+
+    return {
+        winner: winners,
+        processedBallets: processed,
+        optionValues: counted,
+    };
 }
